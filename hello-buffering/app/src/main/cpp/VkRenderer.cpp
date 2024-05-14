2a4636e7cf081672fc03a631a8b85a0adb636ddc
Daemyung Jang <dm86.jang@gmail.com>
2024-05-14 20:20:40 +0900

    Fix resource leaks

--- hello-buffering/app/src/main/cpp/VkRenderer.cpp
+++ hello-buffering/app/src/main/cpp/VkRenderer.cpp
@@ -1237,7 +1237,6 @@ VkRenderer::~VkRenderer() {
     for (auto uniformMemory : mUniformMemories) {
         vkUnmapMemory(mDevice, uniformMemory);
     }
-    mUniformMemories.clear();
     for (auto uniformMemory : mUniformMemories) {
         vkFreeMemory(mDevice, uniformMemory, nullptr);
     }
